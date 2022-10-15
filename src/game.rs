b030e9a3f0bbbebc82707e8f85fb517cb599fd87
0x1A0A <mask.monkey1994@gmail.com>
2022-10-15 20:28:17 +0700

    implement board drawing function

--- src/game.rs
+++ src/game.rs
@@ -1,3 +1,5 @@
+use raylib::prelude::*;
+
 pub mod grid;
 pub mod piece;
 
@@ -11,3 +13,66 @@ pub fn init_game() -> Game {
     };
 }
 
+impl Game {
+    pub fn run(&self) {
+        let (mut rl, thread) = raylib::init()
+            .size(640,480)
+            .title("dfalse - TETRIS")
+            .build();
+
+        while !rl.window_should_close() {
+            let mut d = rl.begin_drawing(&thread);
+
+            d.clear_background(Color::WHITE);
+            d.draw_text("TETRIS", 10,10, 10, Color::GRAY);
+            self.draw_board(&mut d);
+        }
+    }
+
+    fn draw_board(&self, d:&mut RaylibDrawHandle) {
+        const size:i32= 20;
+        const start:(i32,i32) = (10,30);
+
+        // verticle line
+        for i in 2..grid::BOARD_WIDTH-1 {
+            d.draw_line( 
+                size*i as i32 +start.0,
+                start.1,
+                size*i as i32 +start.0,
+                grid::BOARD_HEIGHT as i32 *size +start.1,
+                Color::GRAY
+                );
+        }
+
+        // horizontal line
+        for i in 1..grid::BOARD_HEIGHT-1 {
+            d.draw_line( 
+                start.0,
+                size*i as i32 +start.1,
+                grid::BOARD_WIDTH as i32 * size +start.0,
+                size*i as i32 +start.1,
+                Color::GRAY
+                );
+        }
+
+        //draw block
+        for i in 0..grid::BOARD_HEIGHT {
+            for j in 0..grid::BOARD_WIDTH {
+                match self.board.get(j,i) {
+                   grid::GridCell::EMPTY => {
+                   },
+                   grid::GridCell::WALL => {
+                       d.draw_rectangle(
+                           start.0 + j as i32 * size,
+                           start.1 + i as i32 * size,
+                           size, size,
+                           Color::BLACK
+                           );
+                   },
+                   grid::GridCell::BLOCK => {
+                   },
+                }
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,5 @@ mod game;
 
 fn main() {
     let tetris = game::init_game();
-    tetris.board.print_debug();
-    let r : game::piece::PieceType = rand::random();
-    let p = game::piece::Pieces::make(r);
-    p.print_debug();
+    tetris.run();
 }

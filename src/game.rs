93936e4210c83ef89a8eb19bcc750fe388233835
0x1A0A <mask.monkey1994@gmail.com>
2022-10-18 15:01:32 +0700

    add droping mechanic and line completion check, at this state the game now playable

--- src/game.rs
+++ src/game.rs
@@ -7,12 +7,16 @@ pub mod piece;
 pub struct Game {
     pub board : grid::Grid,
     pub play_piece : Option<piece::Pieces>,
+    drop_time: f64,
+	down_hold: f64,
 }
 
 pub fn init_game() -> Game {
     return Game {
         board : grid::build_grid(), 
         play_piece : None,
+        drop_time: 0.0,
+		down_hold: 0.0,
     };
 }
 
@@ -36,13 +40,23 @@ impl Game {
                 Some(_) => {
                 },
             } 
-            
+
             self.handle_slide(&mut rl);
             self.handle_rotate(&mut rl);
-            self.set_board();
+            self.set_board(); 
 
-            if rl.is_key_pressed(KeyboardKey::KEY_SPACE) {
+			if rl.is_key_down(KeyboardKey::KEY_K) {
+				self.down_hold += rl.get_frame_time() as f64;
+			}
+
+            if self.drop_time >= 0.5 || 
+			rl.is_key_pressed(KeyboardKey::KEY_K) || 
+			self.down_hold >= 0.05 {
                 self.move_down();
+                self.drop_time = 0.0;
+				self.down_hold = 0.0;
+            } else {
+                self.drop_time += rl.get_frame_time() as f64;
             }
 
             let mut d = rl.begin_drawing(&thread);
@@ -50,16 +64,18 @@ impl Game {
             d.clear_background(Color::RAYWHITE);
             d.draw_text("TETRIS", 10,10, 10, Color::GRAY);
             self.draw_board(&mut d);
+            d.draw_text(& format!("{}",self.drop_time), 70,10, 10, Color::RED);
         }
     }
-    
+
     fn move_down(&mut self) {
         self.play_piece.as_mut().unwrap().pos.1 += 1;
+
         if self.check_colision() {
+            let p: &mut piece::Pieces = self.play_piece.as_mut().unwrap();
             // can't move down so it is the end of this round 
-            self.play_piece.as_mut().unwrap().pos.1 -= 1;
+            p.pos.1 -= 1;
             // change all moving to block
-            let p: & piece::Pieces = self.play_piece.as_ref().unwrap();
             for i in 0..p.width {
                 for j in 0..p.width {
                     if let grid::GridCell::MOVING = p.get(j,i) {
@@ -68,6 +84,41 @@ impl Game {
                 }
             }
             self.play_piece = None;
+			let mut complete = 0;
+			// completion checking
+			let mut i: isize = grid::BOARD_HEIGHT-2;
+			while i >= 0 {
+			
+				let mut del = true;
+				for j in 1..grid::BOARD_WIDTH-1 {
+					if let grid::GridCell::EMPTY = self.board.get(j,i) {
+						del = false;
+						break;
+					}
+				}
+				
+				if del {
+					complete+=1;
+					for k in (0..=i).rev() {
+						for j in 1..grid::BOARD_WIDTH-1 {
+							if k-1< 0 {
+								self.board.set(j,k, grid::GridCell::EMPTY);
+							} else {
+								let p = self.board.get( j,k - 1);
+								self.board.set(j,k, *p);
+							}
+						}
+					}
+					i+=1;
+				}
+
+				i-=1;
+			}
+			
+
+			if complete != 0 {
+				println!("::{complete} Line Deleted");
+			}
         }
     }
 
@@ -77,7 +128,16 @@ impl Game {
             for j in 1..grid::BOARD_WIDTH-1 {
                 // add moving block to the board
                 if j >= p.pos.0 && j < p.pos.0 + p.width && i >= p.pos.1 && i < p.pos.1 + p.width {
-                   self.board.set(j, i, p.get(j-p.pos.0, i-p.pos.1)); 
+                    if let grid::GridCell::MOVING = p.get(j-p.pos.0, i-p.pos.1) {
+                        self.board.set(j, i, p.get(j-p.pos.0, i-p.pos.1)); 
+                    } else {
+                        match self.board.get(j,i) {
+                            grid::GridCell::MOVING => {
+                                self.board.set(j,i,grid::GridCell::EMPTY);
+                            },
+                            _ => {},
+                        }
+                    }
                 } else {
                     // remove old moving block from the board
                     match self.board.get(j,i) {
@@ -90,11 +150,11 @@ impl Game {
             }
         }
     }
-    
+
     fn handle_slide(&mut self, rl:&mut RaylibHandle) {
         let mv =
             rl.is_key_pressed(KeyboardKey::KEY_L) as isize - 
-            rl.is_key_pressed(KeyboardKey::KEY_H) as isize;
+            rl.is_key_pressed(KeyboardKey::KEY_J) as isize;
 
         if mv == 0 { return; }
 
@@ -113,8 +173,8 @@ impl Game {
                 if let grid::GridCell::MOVING = p.get(j,i) {
                     if let grid::GridCell::WALL | grid::GridCell::BLOCK =
                         self.board.get(j+p.pos.0,i+p.pos.1) {
-                        return true;
-                    }
+                            return true;
+                        }
                 }
             }
         }
@@ -161,16 +221,16 @@ impl Game {
         for i in 0..grid::BOARD_HEIGHT {
             for j in 0..grid::BOARD_WIDTH {
                 match self.board.get(j,i) {
-                   grid::GridCell::EMPTY => {
-                   },
-                   _ => {
-                       d.draw_rectangle(
-                           START.0 + j as i32 * SIZE,
-                           START.1 + i as i32 * SIZE,
-                           SIZE, SIZE,
-                           Color::GRAY
-                           );
-                   },
+                    grid::GridCell::EMPTY => {
+                    },
+                    _ => {
+                        d.draw_rectangle(
+                            START.0 + j as i32 * SIZE,
+                            START.1 + i as i32 * SIZE,
+                            SIZE, SIZE,
+                            Color::GRAY
+                            );
+                    },
                 }
             }
         }
